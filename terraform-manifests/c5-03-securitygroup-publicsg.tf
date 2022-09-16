7949af31532699b7c84f7f3e560b332bf15d0c5f
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 17:47:31 -0400

    Update c5-03-securitygroup-publicsg.tf

--- terraform-manifests/c5-03-securitygroup-publicsg.tf
+++ terraform-manifests/c5-03-securitygroup-publicsg.tf
@@ -1,12 +1,12 @@
 # AWS EC2 Security Group Terraform Module
 # Security Group for Public Subnet
-module "public_sg" {
+module "sg-public" {
   source  = "terraform-aws-modules/security-group/aws"
   #version = "3.18.0"
   version = "4.0.0"
 
   #name = "public-sg"  
-  name = "${local.name}-public-sg"
+  name = "${local.name}sg-public"
   description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id
   # Ingress Rules & CIDR Blocks
