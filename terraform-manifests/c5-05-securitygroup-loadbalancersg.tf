709a52f4a981636c0f84827824791293ba43deae
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 17:49:24 -0400

    Update c5-05-securitygroup-loadbalancersg.tf

--- terraform-manifests/c5-05-securitygroup-loadbalancersg.tf
+++ terraform-manifests/c5-05-securitygroup-loadbalancersg.tf
@@ -1,11 +1,11 @@
 # Security Group for Public Load Balancer
-module "loadbalancer_sg" {
+module "SG_Loadbalancer" {
   source  = "terraform-aws-modules/security-group/aws"
   #version = "3.18.0"
   version = "4.0.0"
 
   #name = "loadbalancer-sg"
-  name = "${local.name}-loadbalancer-sg"  
+  name = "${local.name}SG-Loadbalancer"  
   description = "Security Group with HTTP open for entire Internet (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id
   # Ingress Rules & CIDR Blocks
