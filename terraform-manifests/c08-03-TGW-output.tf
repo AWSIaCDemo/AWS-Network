9408553e9ad963a4965b245df00e91ac863b2896
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 18:22:00 -0400

    Add files via upload

---
+++ terraform-manifests/c08-03-TGW-output.tf
@@ -0,0 +1,18 @@
+/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+   SPDX-License-Identifier: MIT-0 */
+
+output "tgw" {
+  description = "TGW ID for VPC attachments"
+  value       = aws_ec2_transit_gateway.org_tgw.id
+}
+
+output "tgw_route_table" {
+  description = "Map of route tables used for association and propagation"
+  value = tomap({
+    for k, rt in aws_ec2_transit_gateway_route_table.org_tgw : k => rt.id
+  })
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Term
