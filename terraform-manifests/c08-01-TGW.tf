383fdd5b318439af65344b892d48990f4cec9d77
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 19:28:52 -0400

    Update c08-01-TGW.tf

--- terraform-manifests/c08-01-TGW.tf
+++ terraform-manifests/c08-01-TGW.tf
@@ -1,7 +1,7 @@
 resource "aws_ec2_transit_gateway" "Network-Prod-E1-TGW" {
   description                     = "Network-transit-gateway"
   amazon_side_asn                 = 64512
-  auto_accept_shared_attachments  = "disable"
+  auto_accept_shared_attachments  = "enable"
   default_route_table_association = "enable"
   default_route_table_propagation = "enable"
   dns_support                     = "enable"
