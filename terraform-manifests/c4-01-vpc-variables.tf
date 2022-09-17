044e7ae0dc4be6263f24566ef1c3b1df2f9065d0
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 23:37:52 -0400

    Update c4-01-vpc-variables.tf

--- terraform-manifests/c4-01-vpc-variables.tf
+++ terraform-manifests/c4-01-vpc-variables.tf
@@ -22,21 +22,21 @@ variable "vpc_availability_zones" {
 }
 
 # VPC Public Subnets
-variable "network_prod_public_e1_sbnt" {
+variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
   default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 # VPC Private Subnets
-variable "network_prod_private_e1_sbnt" {
+variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
   default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 # VPC Database Subnets
-variable "network_prod_db_e1_sbnt" {
+variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
   default = ["10.0.151.0/24", "10.0.152.0/24"]
