53434ab8830317bde7ded02112b0bcddfc3c2827
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 22:42:52 -0400

    Update c08-01-TGW.tf

--- terraform-manifests/c08-01-TGW.tf
+++ terraform-manifests/c08-01-TGW.tf
@@ -1,62 +1,78 @@
-resource "aws_ec2_transit_gateway" "org_tgw" {
-  description                     = "Org TGW with auto accept shared for prod, dev and shared environments"
-  auto_accept_shared_attachments  = "enable"
-  default_route_table_association = "disable"
-  default_route_table_propagation = "disable"
-  tags = {
-    Name = "Org_TGW_${var.environment}"
+resource "aws_ec2_transit_gateway" "this" {
+  amazon_side_asn                 = var.amazon_side_asn
+  auto_accept_shared_attachments  = var.auto_accept_shared_attachments
+  default_route_table_association = var.default_route_table_association
+  default_route_table_propagation = var.default_route_table_propagation
+  description                     = var.description
+  dns_support                     = var.dns_support
+  tags                            = var.tags
+  vpn_ecmp_support                = var.vpn_ecmp_support
+}
+
+module "route_tables" {
+  source   = "./modules/route-table"
+  for_each = { for route_table in var.route_tables : route_table.name => route_table }
+
+  transit_gateway_id = aws_ec2_transit_gateway.this.id
+
+  tags = merge(
+    { "Name" : each.key },
+    each.value.tags,
+  )
+}
+
+module "routes" {
+  source   = "./modules/route"
+  for_each = { for route in var.routes : route.name => route }
+
+  blackhole              = each.value.blackhole
+  destination_cidr_block = each.value.destination_cidr_block
+
+  transit_gateway_route_table_id = each.value.default_route_table ? (
+    aws_ec2_transit_gateway.this.association_default_route_table_id) : (
+    try(
+      module.route_tables[each.value.transit_gateway_route_table].route_table.id,
+      each.value.transit_gateway_route_table
+    )
+  )
+
+  transit_gateway_attachment_id = try(
+    module.vpc_attachments[each.value.transit_gateway_attachment].vpc_attachment.id,
+    each.value.transit_gateway_attachment
+  )
+}
+
+module "vpc_attachments" {
+  source   = "./modules/vpc-attachment"
+  for_each = { for attachment in var.vpc_attachments : attachment.name => attachment }
+
+  subnet_ids             = each.value.subnet_ids
+  transit_gateway_id     = aws_ec2_transit_gateway.this.id
+  appliance_mode_support = each.value.appliance_mode_support
+  dns_support            = each.value.dns_support
+  ipv6_support           = each.value.ipv6_support
+  vpc_routes             = each.value.vpc_routes
+
+  transit_gateway_default_route_table_association = each.value.transit_gateway_default_route_table_association
+  transit_gateway_default_route_table_propagation = each.value.transit_gateway_default_route_table_propagation
+
+  transit_gateway_route_table_association = each.value.transit_gateway_route_table_association == null ? null : {
+    transit_gateway_route_table_id = try(
+      module.route_tables[each.value.transit_gateway_route_table_association].route_table.id,
+      each.value.transit_gateway_route_table_association
+    )
   }
-}
 
-resource "aws_ram_resource_share" "main" {
-  name                      = "tgw-org-share"
-  allow_external_principals = false
+  transit_gateway_route_table_propagations = [for route_table in each.value.transit_gateway_route_table_propagations : {
+    name = route_table
+    transit_gateway_route_table_id = try(
+      module.route_tables[route_table].route_table.id,
+      route_table
+    )
+  }]
 
-  tags = {
-    Name = "org-tgw-ram-share"
-  }
-}
-
-# Requires RAM enabled to share with AWS org:
-# enable in org master account with 'aws ram enable-sharing-with-aws-organization'
-resource "aws_ram_principal_association" "org" {
-  principal          = var.org_arn
-  resource_share_arn = aws_ram_resource_share.main.arn
-}
-
-# Requires RAM enabled to share with AWS org
-resource "aws_ram_resource_association" "tgw" {
-  resource_arn       = aws_ec2_transit_gateway.org_tgw.arn
-  resource_share_arn = aws_ram_resource_share.main.arn
-}
-
-resource "aws_ec2_transit_gateway_route_table" "org_tgw" {
-  for_each           = toset(var.tgw_route_tables)
-  transit_gateway_id = aws_ec2_transit_gateway.org_tgw.id
-  tags = {
-    Name = each.value
-  }
-}
-
-resource "aws_ec2_transit_gateway_route" "blackhole_route" {
-  for_each                       = aws_ec2_transit_gateway_route_table.org_tgw
-  destination_cidr_block         = "10.0.104.0/24"
-  blackhole                      = true
-  transit_gateway_route_table_id = each.value.id
-}
-
-resource "aws_ec2_transit_gateway_route" "default_route" {
-  for_each                       = aws_ec2_transit_gateway_route_table.org_tgw
-  destination_cidr_block         = "0.0.0.0/0"
-  transit_gateway_attachment_id  = var.inspection_attachment
-  blackhole                      = false
-  transit_gateway_route_table_id = each.value.id
-}
-
-resource "aws_ec2_transit_gateway_route" "default_route_ipv6" {
-  for_each                       = aws_ec2_transit_gateway_route_table.org_tgw
-  destination_cidr_block         = "::/0"
-  transit_gateway_attachment_id  = var.inspection_attachment
-  blackhole                      = false
-  transit_gateway_route_table_id = each.value.id
+  tags = merge(
+    { "Name" : each.key },
+    each.value.tags,
+  )
 }
