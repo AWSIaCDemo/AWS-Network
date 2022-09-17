ea71268f845821c33490dc3c58b445ebc678040b
AWSIaCDemo <90651730+AWSIaCDemo@users.noreply.github.com>
2022-09-16 22:43:58 -0400

    Update c08-03-TGW-output.tf

--- terraform-manifests/c08-03-TGW-output.tf
+++ terraform-manifests/c08-03-TGW-output.tf
@@ -1,11 +1,19 @@
-output "tgw" {
-  description = "TGW ID for VPC attachments"
-  value       = aws_ec2_transit_gateway.org_tgw.id
+output "transit_gateway" {
+  description = "Object with attributes of the Transit Gateway"
+  value       = aws_ec2_transit_gateway.this
 }
 
-output "tgw_route_table" {
-  description = "Map of route tables used for association and propagation"
-  value = tomap({
-    for k, rt in aws_ec2_transit_gateway_route_table.org_tgw : k => rt.id
-  })
+output "vpc_attachments" {
+  description = "Map of TGW peering attachment objects"
+  value       = module.vpc_attachments
+}
+
+output "route_tables" {
+  description = "Map of TGW route table objects"
+  value       = module.route_tables
+}
+
+output "routes" {
+  description = "Map of TGW route objects"
+  value       = module.routes
 }
